a1f96df31e3d0b0a2d21502cfe223e75fb1e3e47
Lenovo <viktoryia.vayavodava@gmail.com>
2018-08-12 19:42:02 +0200

    some updates

---
+++ src/test/java/SlotMachineEightLinesTest.java
@@ -0,0 +1,103 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class SlotMachineEightLinesTest {
+    private WebDriver driver;
+    private SlotGame slotGame;
+    private MachineFooter machineFooter;
+    private ChangeFooter changeFooter;
+
+    @Before
+    public void setUp() throws IOException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\IdeaProjects\\netEntTask\\src\\main\\resources\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://slotmachinescript.com/");
+        slotGame = new SlotGame(driver);
+        machineFooter = new MachineFooter(driver);
+        changeFooter = new ChangeFooter(driver);
+        changeFooter.changeMachine(2);
+
+    }
+
+    @Test
+    public void betUpTest (){
+        int initialBet = machineFooter.getBetAmt();
+        machineFooter.setBetUp(1);
+        int newBet = machineFooter.getBetAmt();
+        Assert.assertFalse(initialBet==newBet);
+    }
+
+    @Test
+    public void betDownTest (){
+        machineFooter.setBetUp(1);
+        int initialBet = machineFooter.getBetAmt();
+        machineFooter.setBetDown(1);
+        int newBet = machineFooter.getBetAmt();
+        Assert.assertFalse(initialBet==newBet);
+    }
+
+    @Test
+    public void betResetTest (){
+        int initialBet = machineFooter.getBetAmt();
+        machineFooter.setBetUp(3);
+        machineFooter.setBetDown(3);
+        int newBet = machineFooter.getBetAmt();
+        Assert.assertTrue(initialBet==newBet);
+    }
+
+    @Test
+    public void betMinimumTest(){
+        int initialBet = machineFooter.getBetAmt();
+        machineFooter.setBetDown(2);
+        int newBet = machineFooter.getBetAmt();
+        Assert.assertTrue(initialBet==newBet);
+    }
+
+    @Test
+    public void betMaxTest(){
+        int initialBet = machineFooter.getBetAmt();
+        machineFooter.setBetUp(20);
+        int newBet = machineFooter.getBetAmt();
+        Assert.assertEquals(newBet,10);
+    }
+
+    @Test
+    public void spinsLeftTest(){
+        int currentSpins = machineFooter.getSpinsAmt();
+        int currentBet = machineFooter.getBetAmt();
+        machineFooter.spinTheReels();
+        machineFooter.calculateSpinsLeft (currentSpins,currentBet);
+    }
+
+    @Test
+    public void winChartChangeTest (){
+        int currentBet = machineFooter.getBetAmt();
+        int [] currentPayouts = slotGame.payoutsEight;
+        machineFooter.setBetUp(1);
+        int [] payoutsCalculated = slotGame.calculatePayoutsEight(currentBet,currentPayouts);
+        int[] newPayouts = slotGame.payoutsEight;
+        for (int i=0;i<currentPayouts.length;i++){
+            Assert.assertTrue(newPayouts[i]==payoutsCalculated[i]);
+        }
+    }
+
+    @After
+    public void tearDown(){
+        driver.manage().deleteAllCookies();
+        driver.quit();
+        driver=null;
+
+    }
+
+}
